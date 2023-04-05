149915983151ad1069c6b805efa54c50e1c09822
Plum <plumlulz@gmail.com>
2023-04-04 23:33:06 -0600

    Added query command

--- mysqldumper/msdpy/mysql.py
+++ mysqldumper/msdpy/mysql.py
@@ -106,6 +106,7 @@ try:
 	resp = req.json()
 except ValueError:
 	print("No JSON was received. Check all parameters to make sure they are correct.")
+	print(req.text)
 	exit()
 if resp['response_code'] == 0:
 	print("Something happened. Response message: "+resp['response'])
@@ -141,20 +142,24 @@ elif resp['response_code'] == 1:
 						table.align = "l"
 						for line in resp['response'].splitlines():
 							table.add_row([line])
-					else:
+						print(table)
+					elif resp['response_code'] == 0:
 						table.field_names = ["Database", "Size"]
 						table.align = "l"
 						for line in resp['response'].splitlines():
 							spl = line.split(":")
 							table.add_row([spl[0], spl[1]])	
-					print(table)
+						print(table)
+					elif resp['response_code'] == 5:
+						# JSON error
+						print (resp['response'])
 				except SystemExit:
 					None
 			elif input_var[0:11] == 'show tables':
 				args = input_var[11:]
-				parser = argparse.ArgumentParser(description='Show all tables from a database..', prog='show tables')
+				parser = argparse.ArgumentParser(description='Show all tables from a database.', prog='show tables')
 				if currentdb == None:
-					parser.add_argument("database", help='Database to show tables from.', default=currentdb)
+					parser.add_argument("database", help='Database to show tables from.')
 				g = parser.add_mutually_exclusive_group()
 				g.add_argument("-count", help='Display row count of table.', action='store_true')
 				g.add_argument("-size", help='Display estimated size of table.', action='store_true')
@@ -178,24 +183,30 @@ elif resp['response_code'] == 1:
 					table = PrettyTable()
 					if resp['response_code'] == 0:
 						table = resp['response']
+						print (table)
 					elif resp['response_code'] == 1:
 						table.field_names = ["Tables in "+database]
 						table.align = "l"
 						for line in resp['response'].splitlines():
 							table.add_row([line])
+						print (table)
 					elif resp['response_code'] == 2:
 						table.field_names = ["Tables in "+database, "Row Count"]
 						table.align = "l"
 						for line in resp['response'].splitlines():
 							spl = line.split(":")
 							table.add_row([spl[0], '{:,}'.format(int(spl[1]))])
+						print (table)
 					elif resp['response_code'] == 3:
 						table.field_names = ["Tables in "+database, "Size of Table"]
 						table.align = "l"
 						for line in resp['response'].splitlines():
 							spl = line.split(":")
 							table.add_row([spl[0], spl[1]])
-					print (table)
+						print (table)
+					elif resp['response_code'] == 5:
+						# JSON error
+						print(resp['response'])
 				except SystemExit:
 					None
 			elif input_var[0:10] == 'dump table':
@@ -219,7 +230,7 @@ elif resp['response_code'] == 1:
 					if resp['response_code'] == 0:
 						# Error something happened with MySQL Query; print MySQL Error
 						print(resp['response'])
-					if resp['response_code'] == 2:
+					elif resp['response_code'] == 2:
 						# Table under 10,000 rows was dumped
 						print(resp['response'])
 						postdata2 = postdata.copy()
@@ -272,6 +283,9 @@ elif resp['response_code'] == 1:
 
 						if confirm("Download '%s'?" % (writefile)):
 							download(writefile)
+					elif resp['response_code'] == 5:
+						# JSON error
+						print(resp['response'])
 				except SystemExit:
 					None
 			elif input_var[0:13] == 'dump database':
@@ -349,6 +363,9 @@ elif resp['response_code'] == 1:
 
 						if confirm("Download '%s'?" % (writefile)):
 							download(writefile)
+					elif resp['response_code'] == 5:
+						# JSON error
+						print(resp['response'])
 				except SystemExit:
 					None
 			elif input_var[0:12] == 'use database':
@@ -371,15 +388,71 @@ elif resp['response_code'] == 1:
 							print("'%s' is not a valid database to use." % (parsed.database))
 				except SystemExit:
 					None
+			elif input_var[0:5] == 'query':
+				args = input_var[5:]
+				parser = argparse.ArgumentParser(description='Execute SQL query.', prog='query')
+				parser.add_argument("sqlquery", help='SQL query to execute.')
+				if currentdb == None:
+					parser.add_argument("database", help='Database to execute query from.')
+				parser.add_argument("-delimiter", help='Split results by delimiter instead of in a table.')
+				g = parser.add_mutually_exclusive_group(required=True)
+				g.add_argument("-f", help='Download query result to file. (Good for large returns)')
+				g.add_argument("-p", help='Print query result to screen. (Good for small returns)', action='store_true')
+
+
+				try:
+					parsed = parser.parse_args(shlex.split(args))
+					if currentdb == None:
+						database = parsed.database
+					else:
+						database = currentdb
+					postdata2.update({"sql_query": 0, "query": parsed.sqlquery, "database": database})
+					req = requests.post(url, data=postdata2)
+					resp = req.json()
+					if resp['response_code'] == 0:
+						# Error
+						print(resp['response'])
+					elif resp['response_code'] == 2:
+						# No data returned but rows were affected
+						print("Number of rows affected: %s" % (resp['response']))
+					elif resp['response_code'] == 1:
+						data = json.loads(resp['response'])
+
+						# Create table and list for field names
+						if parsed.delimiter == None:
+							table = PrettyTable()
+							table.field_names = list(data[0].keys())
+							table.align = "l"
+						else:
+							table = parsed.delimiter.join(list(data[0].keys()))+"\n"
+						for line in data:
+							if parsed.delimiter == None:
+								table.add_row(list(line.values()))
+							else:
+								table += parsed.delimiter.join(list(map(str, line.values())))+"\n"
+						if parsed.p:
+							print("Row count: %s" % (len(data)))
+							print(table)
+						elif parsed.f:
+							with open(parsed.f, "w+") as outfile:
+								outfile.write("Row count: %s\n" % (len(data)))
+								outfile.write(str(table))
+							print("Wrote query results to: %s" % (parsed.f))
+					elif resp['response_code'] == 5:
+						# JSON error
+						print(resp['response'])
+				except SystemExit:
+					None
 			elif input_var == 'help':
 				table = PrettyTable()
 				table.field_names = ["Command", "Description", "Flags"]
 				table.align = "l"
 				table.add_row(["show databases", "Display all databases on MySQL server.", "-size"])
-				table.add_row(["show tables", "Display all tables from a database.", "database*, -size, -count"])
-				table.add_row(["dump table", "Dump table from a database.", "table, database*"])
-				table.add_row(["dump database", "Dump entire database.", "database*"])
+				table.add_row(["show tables", "Display all tables from a database.", "*database*, -size, -count"])
+				table.add_row(["dump table", "Dump table from a database.", "table, *database"])
+				table.add_row(["dump database", "Dump entire database.", "*database"])
 				table.add_row(["use database", "Set database to use for session.", "database"])
+				table.add_row(["query", "Execute SQL query. Print or write to file results.", "*database, -delimiter, -f, -p"])
 				table.add_row(["clear", "Clears console.", "No flags"])
 
 				print(table)
@@ -387,5 +460,4 @@ elif resp['response_code'] == 1:
 			elif input_var[0:5] == "clear":
 				print("\x1b[2J\x1b[-1;-1H", end="")
 		except KeyboardInterrupt:
-			print ("\nBye")
 			break
